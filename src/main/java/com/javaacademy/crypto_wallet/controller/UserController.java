34e4389f1b10ce07501bd9795161c32b45e87626
no_fedov <nefedov.evgeniy.d@yandex.ru>
2024-12-08 18:38:23 +0300

    feat: реализовать эндпоинты сохранения пользователя и обновления пароля

--- src/main/java/com/javaacademy/crypto_wallet/controller/UserController.java
+++ src/main/java/com/javaacademy/crypto_wallet/controller/UserController.java
@@ -7,9 +7,11 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,6 +23,7 @@ public class UserController {
     private final UserServiceIF userService;
 
     @PostMapping("/signup")
+    @ResponseStatus(HttpStatus.CREATED)
     public void saveUser(@RequestBody @Valid UserCreateDto userCreateDto) {
         log.info("POST: user/signup, new user = {}", userCreateDto);
         userService.save(userCreateDto);
@@ -28,6 +31,7 @@ public class UserController {
     }
 
     @PostMapping("/reset-password")
+    @ResponseStatus(HttpStatus.CREATED)
     public void updatePassword(@RequestBody @Valid UserUpdatePasswordDto userUpdatePasswordDto) {
         log.info("POST: user/reset-password, updated user = {}", userUpdatePasswordDto);
         userService.resetPassword(userUpdatePasswordDto);
